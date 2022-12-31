54ade898f8a7909d726ddc228399616c85402aef
itschip <chip@projecterror.dev>
2022-12-31 13:14:02 +0100

    perfection.

--- lua/zenbones-config/init.lua
+++ lua/zenbones-config/init.lua
@@ -1,8 +1,10 @@
 vim.opt.background = "dark"
 
 vim.g.zenbones_transparent_background = false
-vim.g.tokyobones_transparent_background = false
+vim.g.tokyobones_transparent_background = true
 vim.g.rosebones_transparent_background = false
-vim.g.seoulbones_transparent_background = false
+vim.g.seoulbones_transparent_background = true
 
-vim.cmd("colorscheme duckbones")
+vim.cmd("colorscheme seoulbones")
+
+vim.cmd("hi CursorLine cterm=underline term=underline ctermbg=NONE guibg=NONE")
