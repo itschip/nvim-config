d2b66b7d6e7aa7f298e3d124b34ea1c6c3410edc
itschip <christopher.gjelten@wilhelmsen.com>
2023-10-02 10:06:13 +0200

    add todo stufF

--- lua/lazy-config/init.lua
+++ lua/lazy-config/init.lua
@@ -27,6 +27,15 @@ require("lazy").setup({
 			})
 		end,
 	},
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+	},
 	"saadparwaiz1/cmp_luasnip",
 	"jose-elias-alvarez/null-ls.nvim",
 	{
