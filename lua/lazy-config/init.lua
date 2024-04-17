8fb52b784ecbf98fc7b9712cdca3f79a23c199dc
itschip <chip@projecterror.dev>
2024-04-17 23:20:33 +0200

    best theme

--- lua/lazy-config/init.lua
+++ lua/lazy-config/init.lua
@@ -72,13 +72,35 @@ require("lazy").setup({
 	"nvim-lualine/lualine.nvim",
 	"tpope/vim-fugitive",
 	{
-        "catppuccin/nvim",
-        name = "catppuccin",
+        "rebelot/kanagawa.nvim",
 		config = function()
-            require("catppuccin").setup({
-                transparent_background = true
-            })
-            vim.cmd("colorscheme catppuccin")
+require('kanagawa').setup({
+    compile = false,             -- enable compiling the colorscheme
+    undercurl = true,            -- enable undercurls
+    commentStyle = { italic = false },
+    functionStyle = {},
+    keywordStyle = { italic = false },
+    statementStyle = { bold = true },
+    typeStyle = {},
+    transparent = true,         -- do not set background color
+    dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
+    terminalColors = true,       -- define vim.g.terminal_color_{0,17}
+    colors = {                   -- add/modify theme and palette colors
+        palette = {},
+        theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+    },
+    overrides = function(colors) -- add/modify highlights
+        return {}
+    end,
+    theme = "dragon",              -- Load "wave" theme when 'background' option is not set
+    background = {               -- map the value of 'background' option to a theme
+        dark = "dragon",           -- try "dragon" !
+        light = "lotus"
+    },
+})
+
+-- setup must be called before loading
+vim.cmd("colorscheme kanagawa")
 		end,
 	},
 })
